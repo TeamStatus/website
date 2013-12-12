96095b239547d1a4e74fab51c35a11716d2d6bb1
Pawel Niewiadomski <11110000b@gmail.com>
2013-12-12 20:53:06 +0100

    removing sinatra app

--- assets/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-//= require global.js
-$(function() {
-	if (mixpanel) {
-		mixpanel.track("Home");
-		$('.container.marketing a[data-mixpanel]').click(function() {
-			mixpanel.track('Sign up clicked', {'Selected flavor' : $(this).data('mixpanel')});
-		});
-	}
-});
