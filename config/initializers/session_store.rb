b83d4c522f855af727bac0df758d3286451feb1c
Pawel Niewiadomski <11110000b@gmail.com>
2013-12-15 08:27:10 +0100

    configured cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ConsoleRails::Application.config.session_store :cookie_store, key: '_console-rails_session'
+ConsoleRails::Application.config.session_store :cookie_store, key: ENV['COOKIE_NAME'], domain: ENV['COOKIE_DOMAIN'], secret_key_base: ENV['COOKIE_SECRET']
