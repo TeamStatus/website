d38e61d950f3ed510fc4c5d27cd40832438e2da8
Pawel Niewiadomski <11110000b@gmail.com>
2013-12-20 05:59:31 +0100

    Revert "unicorn.rb edited online with Bitbucket"
    This reverts commit b62cea81337bf3843571204be296416b5fdaa23d.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,8 +16,6 @@ after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-  
-  Mongoid::Sessions.default.disconnect
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
