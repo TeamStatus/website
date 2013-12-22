c203b05865b452cad7730e5311755c0e3d77ed39
Pawel Niewiadomski <11110000b@gmail.com>
2013-12-22 21:37:31 +0100

    fixed partials/integrations/ to be loaded, removed anaylitcs.turbolinks - not going to use turbolinks

--- app/controllers/partials_controller.rb
+++ app/controllers/partials_controller.rb
@@ -4,12 +4,8 @@ class PartialsController < ApplicationController
 		render partial: params[:partial_id]
 	end
 
-	def widget_js
-		render :layout => false, :file => Rails.root.join('integrations', params[:widget_id], 'config.js'), :content_type => 'application/javascript'
-	end
-
 	def widget
-		render :layout => false, :file => Rails.root.join('integrations', params[:widget_id], 'config.html'), :content_type => 'text/html'
+		render "integrations/#{params[:widget_id]}/config", :layout => false , :content_type => 'text/html'
 	end
 
 end
