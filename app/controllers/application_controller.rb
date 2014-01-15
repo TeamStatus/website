737817d613a6445a7c18244b0854efbe444a662d
Pawel Niewiadomski <11110000b@gmail.com>
2014-01-15 06:41:17 +0100

    fixed XSFR cookie domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,12 @@ class ApplicationController < ActionController::Base
 		end
 
 		def set_csrf_cookie_for_ng
-			cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+			if protect_against_forgery?
+				cookies['XSRF-TOKEN'] = {
+					value: form_authenticity_token,
+					domain: ENV['COOKIE_DOMAIN']
+				}
+			end
 		end
 
 		def verified_request?
