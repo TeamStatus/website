5b29bc21e456104625522234910d25bf07d706d3
Pawel Niewiadomski <11110000b@gmail.com>
2014-02-08 16:11:18 +0100

    added migration task that will set plan start/end date and plan type

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,9 @@ class User
   field :male, type: Boolean
   field :created_at, type: Time, default: -> { Time.now }
 
+  field :plan_started_at, type: Time, default: -> { Time.now }
+  field :plan_ending_at, type: Time, default: -> { 30.days.from_now }
+  field :plan_type, type: String, default: 'trial'
+
   index({ email: 1 }, { unique: true })
 end
