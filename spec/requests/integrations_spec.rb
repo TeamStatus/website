af10f38c343eeca776d59dfbc23bf30b90ffa49b
Pawel Niewiadomski <11110000b@gmail.com>
2014-02-15 20:40:16 +0100

    removing unused tests

--- spec/requests/integrations_spec.rb
+++ spec/requests/integrations_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "Integrations" do
 	%w{bamboo-builds jira-issue-list}.each do |widget|
-	  describe "GET /partials/integrations/#{widget}" do
+	  describe "GET /integrations/partials/#{widget}" do
 	    it "works! (now write some real specs)" do
 	      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-	      get "/partials/integrations/#{widget}"
+	      get "/integrations/partials/#{widget}"
 	      response.status.should be(200)
 	    end
 	  end
