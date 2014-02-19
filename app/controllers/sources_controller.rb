0901a33f0306ce88405336283231de6be1ba567e
Pawel Niewiadomski <11110000b@gmail.com>
2014-02-19 12:37:08 +0100

    fixed source controller

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -4,7 +4,7 @@ class SourcesController < ApplicationController
 	before_action :set_source, only: [:show, :edit, :update, :destroy]
 
 	def tap
-		boards_engine.tap(params[:id], ActiveSupport::JSON.decode request.body)
+		# boards_engine.tap(params[:id], ActiveSupport::JSON.decode request.body)
 	end
 
 end
