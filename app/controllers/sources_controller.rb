3e6095ccfe0dbfb948b2a094958a8e57650a1a95
Pawel Niewiadomski <11110000b@gmail.com>
2014-02-20 07:24:46 +0100

    BOARDS_URL is not needed (except production), because boards have a fixed URL now, removed session setting in widgets_spec - those file should be available for anonymous users

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,10 +1,11 @@
 class SourcesController < ApplicationController
 	include BoardsHelper
 
+	skip_before_filter :load_user
 	before_action :set_source, only: [:show, :edit, :update, :destroy]
 
 	def tap
-		# boards_engine.tap(params[:id], ActiveSupport::JSON.decode request.body)
+		boards_engine.tap(params[:id], ActiveSupport::JSON.decode(request.body))
 	end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,18 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 ConsoleRails::Application.initialize!
 
-%w{COOKIE_SECRET COOKIE_NAME MONGODB_URL CONSOLE_SECRET ENCRYPTED_FIELDS_SALT ENCRYPTED_FIELDS_PASSWORD BOARDS_URL CONSOLE_URL}.each do |var|
+%w{COOKIE_SECRET COOKIE_NAME MONGODB_URL CONSOLE_SECRET ENCRYPTED_FIELDS_SALT ENCRYPTED_FIELDS_PASSWORD CONSOLE_URL}.each do |var|
   abort("missing env var: please set #{var}") unless ENV[var]
 end
 
 unless Rails.env.standalone?
-	%w{GOOGLE_KEY GOOGLE_SECRET}.each do |var|
+	%w{GOOGLE_KEY GOOGLE_SECRET BOARDS_URL}.each do |var|
 	  abort("missing env var: please set #{var}") unless ENV[var]
 	end
 
 	ENV['BOARDS_URL'] = ENV['BOARDS_URL'].chomp('/')
+else
+	ENV['BOARDS_URL'] = 'http://127.0.0.1:57518'
 end
 
 ENV['COOKIE_DOMAIN'] ||= ''
