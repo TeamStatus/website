6a391b9b48a9bfb62ed0626debd52f720b81f5fa
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-23 08:04:07 +0100

    removed ENCRYPTED_FIELDS_PASSWORD

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 ConsoleRails::Application.initialize!
 
-%w{COOKIE_SECRET COOKIE_NAME ENCRYPTED_FIELDS_PASSWORD MAILCHIMP_KEY MAILCHIMP_LIST}.each do |var|
+%w{COOKIE_SECRET COOKIE_NAME MAILCHIMP_KEY MAILCHIMP_LIST}.each do |var|
   abort("missing env var: please set #{var}") unless ENV[var]
 end
 
 ENV['COOKIE_DOMAIN'] ||= ''
-ENV['ENCRYPTED_FIELDS_SALT'] ||= '9Rw2OlpA'
 
 %w{GOOGLE_ANALYTICS GOOGLE_KEY GOOGLE_SECRET INTERCOM_APP_ID INTERCOM_KEY INTERCOM_SECRET MIXPANEL_APP_ID}.each do |var|
 	puts "missing env var (some features will be disabled): #{var}" unless ENV[var]
