b6d57641498a892f627d5e484b4701f9805c6738
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-25 21:46:45 +0100

    handling gracefully situation when job disappeared

--- app/workers/run_job_worker.rb
+++ app/workers/run_job_worker.rb
@@ -2,23 +2,24 @@ class RunJobWorker
 	include Sidekiq::Worker
 
   def perform(job_id)
-  	job = Job.find(job_id)
+  	job = Job.find_by_id(job_id)
+    unless job.nil?
+    	IO.popen("node #{Rails.root}/lib/pullers/standalone.js #{job.jobId}", "w+") do |sub|
+    		sub.write job.settings.to_json
+    		sub.close_write
+    		output = sub.read
+    		sub.close
 
-  	IO.popen("node #{Rails.root}/lib/pullers/standalone.js #{job.jobId}", "w+") do |sub|
-  		sub.write job.settings.to_json
-  		sub.close_write
-  		output = sub.read
-  		sub.close
+    		if $?.to_i == 0
+    			job.last_data = JSON.parse output
 
-  		if $?.to_i == 0
-  			job.last_data = JSON.parse output
+          WebsocketRails[job.board.id].trigger job.id, job.last_data
+    		end
+    	end
 
-        WebsocketRails[job.board.id].trigger job.id, job.last_data
-  		end
-  	end
-
-  	job.last_run_at = Time.now()
-  	job.next_run_at = Time.now() + 1.minute
-  	job.save
+    	job.last_run_at = Time.now()
+    	job.next_run_at = Time.now() + 1.minute
+    	job.save
+    end
   end
 end
