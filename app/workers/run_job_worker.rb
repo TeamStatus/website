e5a74b1981c3f7b463715e30e0610027a49b12ac
Paweł Niewiadomski <pniewiadomski@atlassian.com>
2014-03-26 13:51:16 +0100

    chdir when running node

--- app/workers/run_job_worker.rb
+++ app/workers/run_job_worker.rb
@@ -4,20 +4,22 @@ class RunJobWorker
   def perform(job_id)
   	job = Job.find_by_id(job_id)
     unless job.nil?
-    	IO.popen("node #{Rails.root}/lib/pullers/standalone.js #{job.jobId}", "w+") do |sub|
-    		sub.write job.settings.to_json
-    		sub.close_write
-    		output = sub.read
-    		sub.close
+      Dir.chdir "#{Rails.root}/lib/pullers" do
+      	IO.popen("node ./standalone.js #{job.jobId}", "w+") do |sub|
+      		sub.write job.settings.to_json
+      		sub.close_write
+      		output = sub.read
+      		sub.close
 
-    		if $?.to_i == 0
-    			job.last_data = JSON.parse output
+      		if $?.to_i == 0
+      			job.last_data = JSON.parse output
 
-          WebsocketRails[job.board.id].trigger job.id, job.last_data
-        else
-          logger.warn "There was an error running job #{job.id} #{output}"
-    		end
-    	end
+            WebsocketRails[job.board.id].trigger job.id, job.last_data
+          else
+            logger.warn "There was an error running job #{job.id} #{output}"
+      		end
+      	end
+      end
 
     	job.last_run_at = Time.now()
     	job.next_run_at = Time.now() + 1.minute
