171f807e76ac1056bfdbec325418b172e250c4f8
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-27 06:56:28 +0100

    no need for chdir (conflicts with another one inside sidekiq)

--- app/workers/run_job_worker.rb
+++ app/workers/run_job_worker.rb
@@ -4,26 +4,24 @@ class RunJobWorker
   def perform(job_id)
   	job = Job.find_by_id(job_id)
     unless job.nil?
-      Dir.chdir "#{Rails.root}/tmp" do
-      	IO.popen("#{Rails.root}/lib/pullers/standalone #{job.jobId}", "w+") do |sub|
-      		sub.write job.settings.to_json
-      		sub.close_write
-      		output = sub.read
-      		sub.close
+    	IO.popen("#{Rails.root}/lib/pullers/standalone #{job.jobId}", "w+") do |sub|
+    		sub.write job.settings.to_json
+    		sub.close_write
+    		output = sub.read
+    		sub.close
 
-      		if $?.to_i == 0
-      			job.last_data = JSON.parse output
+    		if $?.to_i == 0
+    			job.last_data = JSON.parse output
 
-            WebsocketRails[job.board.id].trigger job.id, job.last_data
-          else
-            logger.warn "There was an error running job #{job.id} #{output}"
-      		end
-      	end
-      end
-
-    	job.last_run_at = Time.now()
-    	job.next_run_at = Time.now() + 1.minute
-    	job.save
+          WebsocketRails[job.board.id].trigger job.id, job.last_data
+        else
+          logger.warn "There was an error running job #{job.id} #{output}"
+    		end
+    	end
     end
+
+  	job.last_run_at = Time.now()
+  	job.next_run_at = Time.now() + 1.minute
+  	job.save
   end
 end
