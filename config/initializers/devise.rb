a46d3751ff68eca8b8ddcf620c3c64419e211762
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-28 06:06:16 +0100

    disable twitter authentication -doesn't return  the email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,7 +269,7 @@ Devise.setup do |config|
   config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], :scope => 'user,repo:status,admin:repo_hook,read:org'
   config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], :scope => 'userinfo.email, userinfo.profile, plus.me',
     :prompt => 'select_account', :image_aspect_ratio => "square", :image_size => 50
-  config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET'], :secure_image_url => true
+  # config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET'], :secure_image_url => true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
