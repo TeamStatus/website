481201c03f8fd0020853da1df6e414c507e27971
Paweł Niewiadomski <pniewiadomski@atlassian.com>
2014-08-23 10:40:23 +0200

    updating capistrano lock to 3.2.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for Capistrano 3.2.x
+lock '3.2.1'
 
 set :application, 'teamstatus'
 set :repo_url, 'git@bitbucket.org:teamstatus/teamstatus.git'
