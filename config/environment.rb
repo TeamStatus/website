6295ef7cb45d3801a6046371e399570eef413bc2
Pawel Niewiadomski <11110000b@gmail.com>
2015-03-28 21:01:04 +0100

    using Figaro initializer

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 ConsoleRails::Application.initialize!
-
-%w{COOKIE_SECRET COOKIE_NAME MAILCHIMP_KEY MAILCHIMP_LIST}.each do |var|
-  abort("missing env var: please set #{var}") unless ENV[var]
-end
-
-ENV['COOKIE_DOMAIN'] ||= ''
-
-%w{GOOGLE_ANALYTICS GOOGLE_KEY GOOGLE_SECRET INTERCOM_APP_ID INTERCOM_KEY INTERCOM_SECRET MIXPANEL_APP_ID}.each do |var|
-	puts "missing env var (some features will be disabled): #{var}" unless ENV[var]
-end
