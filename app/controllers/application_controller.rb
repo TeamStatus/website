ce2b9c3c2e1cae9736a50da2d20c6bcbe007798d
Pawel Niewiadomski <11110000b@gmail.com>
2015-03-30 22:46:26 +0200

    using angular_rails_csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,23 +7,6 @@ class ApplicationController < ActionController::Base
 
 	before_filter :authenticate_user!
 
-	after_filter :set_csrf_cookie_for_ng
-
-	private
-
-	def set_csrf_cookie_for_ng
-		if protect_against_forgery?
-			cookies['XSRF-TOKEN'] = {
-				value: form_authenticity_token,
-				domain: ENV['COOKIE_DOMAIN']
-			}
-		end
-	end
-
-	def verified_request?
-		super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-	end
-
 	def after_sign_in_path_for(resource)
 		boards_path
 	end
