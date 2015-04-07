07f54d53f572b6b08369944a62019c15d04df5b0
Pawel Niewiadomski <11110000b@gmail.com>
2015-04-07 21:06:06 +0200

    figured out proper configuration for simple authentication token

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,18 +1,20 @@
 module Api
 	class ApiController < ApplicationController
 
-		protect_from_forgery with: :null_session
-
 		respond_to :json
 
 		acts_as_token_authentication_handler_for User, fallback_to_devise: false
+
+		skip_before_filter :verify_authenticity_token
+
+		skip_before_filter :authenticate_user!
 
 		before_filter :parse_request
 
 		private
 
 		def parse_request
-			@json = JSON.parse(request.body.read)
+			@json = JSON.parse(request.body.read) if request.method == 'POST' || request.method == 'PUT'
 		end
 	end
 end
--- app/controllers/api/v1/tap_controller.rb
+++ app/controllers/api/v1/tap_controller.rb
@@ -1,6 +1,12 @@
 module Api 
 	module V1
 		class TapController < ApiController
+
+			def create
+			end
+
+			def index
+			end
 		end
 	end
 end
